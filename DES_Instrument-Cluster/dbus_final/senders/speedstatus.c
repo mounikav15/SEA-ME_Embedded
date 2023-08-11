4f6e3ba38c37e711e1bfa37ce11e66b2628309e0
dongdong <tjehdghks4356@gmail.com>
2023-08-11 14:48:18 +0200

    fix

--- DES_Instrument-Cluster/dbus_final/senders/speedstatus.c
+++ DES_Instrument-Cluster/dbus_final/senders/speedstatus.c
@@ -269,6 +269,8 @@ void read_port(uint8_t *speed, uint8_t *rpm)
                 int disk_rpm = (frame.data[0] << 8) + frame.data[1];
                 *rpm = (uint8_t)((float)disk_rpm / 2.6);
                 *speed = (uint8_t)(*rpm * 0.0034);
+
+                printf("Car RPM: %d\t Car Speed: %d m/s\n", rpm, speed);
             }
         }
     }
