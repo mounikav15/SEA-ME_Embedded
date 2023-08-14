698ea815eb35e1c89691720b20a7a84b6530c1ed
dongdong <tjehdghks4356@gmail.com>
2023-08-14 14:57:27 +0200

    start kalman

--- kalmanfilter_test/speedstatus.c
+++ kalmanfilter_test/speedstatus.c
@@ -48,7 +48,7 @@ typedef struct {
 
 
 void kalmanFilter(double measuredstate, KalmanState* state);
-void matrix_multiply(double A[SIZE][SIZE], double B[SIZE][SIZE], double result[SIZE][SIZE]);
+void matrix_multiply(double A[SIZE][SIZE], double B[SIZE], double result[SIZE]);
 
 
 // CAN configuration
@@ -407,74 +407,15 @@ void *dbusSendThread(void *arg)
     return NULL; // Exit the thread function
 }
 
-
-void matrix_multiply(double A[SIZE][SIZE], double B[SIZE][SIZE], double result[SIZE][SIZE]) {
+void matrix_multiply(double A[SIZE][SIZE], double B[SIZE], double result[SIZE]) {
     for (int i = 0; i < SIZE; i++) {
+        result[i] = 0;  // 초기화
         for (int j = 0; j < SIZE; j++) {
-            result[i][j] = 0;
-            for (int k = 0; k < SIZE; k++) {
-                result[i][j] += A[i][k] * B[k][j];
-            }
+            result[i] += A[i][j] * B[j];
         }
     }
 }
 
-void kalmanFilter_(double measuredstate, double estimation[SIZE], double letterP[SIZE][SIZE], double dt, double renewed_e[SIZE], double renewed_P[SIZE][SIZE]) {
-    double letterA[SIZE][SIZE] = {{1, dt},
-                                  {0, 1}};
-    double letterQ[SIZE][SIZE] = {{0.01, 0},
-                                  {0, 0.01}};
-    double letterH[MEASURE_SIZE][SIZE] = {{1, 0}};
-    double letterR[MEASURE_SIZE] = {50};
-
-    // 1. Predict the state and error covariance
-    double predicted_e[SIZE];
-    double temp_result[SIZE][SIZE];
-    matrix_multiply(letterA, (double (*)[SIZE])estimation, temp_result);
-    for (int i = 0; i < SIZE; i++) {
-        predicted_e[i] = temp_result[i][0];
-    }
-
-    double predicted_P[SIZE][SIZE];
-    matrix_multiply(letterA, letterP, predicted_P);
-
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            predicted_P[i][j] += letterQ[i][j];
-        }
-    }
-
-    // 2. Calculate Kalman Gain
-    double K[SIZE];
-    double denominator = (letterH[0][0] * predicted_P[0][0] + letterH[0][1] * predicted_P[1][0]) * letterH[0][0] 
-                         + (letterH[0][0] * predicted_P[0][1] + letterH[0][1] * predicted_P[1][1]) * letterH[0][1] + letterR[0];
-    for (int i = 0; i < SIZE; i++) {
-        K[i] = (predicted_P[i][0] * letterH[0][0] + predicted_P[i][1] * letterH[0][1]) / denominator;
-    }
-
-    // 3. Update the estimation
-    double y = measuredstate - (letterH[0][0] * predicted_e[0] + letterH[0][1] * predicted_e[1]);
-    for (int i = 0; i < SIZE; i++) {
-        renewed_e[i] = predicted_e[i] + K[i] * y;
-    }
-
-    // 4. Update the error covariance
-    double I[SIZE][SIZE] = {{1, 0},
-                            {0, 1}};
-
-    double KH[SIZE][SIZE];
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            KH[i][j] = K[i] * letterH[0][j];
-        }
-    }
-
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            renewed_P[i][j] = (I[i][j] - KH[i][j]) * predicted_P[i][j];
-        }
-    }
-}
 
 
 void kalmanFilter(double measuredstate, KalmanState* state) {
